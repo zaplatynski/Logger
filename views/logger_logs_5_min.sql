6e267eceac5dbea3329bfd33dae8a836cdbf7bf4
Martin D'Souza <martin@clarifit.com>
2018-05-24 21:12:23 -0600

    #199 Fix issue with logger_logs_5/60_min views including timezone when they shouldnt.

--- views/logger_logs_5_min.sql
+++ views/logger_logs_5_min.sql
@@ -1,5 +1,8 @@
-create or replace force view logger_logs_5_min as
-	select * 
-      from logger_logs 
-	 where time_stamp > systimestamp - (5/1440)
+create or replace force view logger_logs_5_min 
+as
+	select /*+ qb_name(logger_logs_5_min) */
+    ll.* 
+	from logger_logs ll
+	where 1=1
+    and ll.time_stamp > cast(systimestamp as timestamp) - (5/1440)
 /
--- views/logger_logs_60_min.sql
+++ views/logger_logs_60_min.sql
@@ -1,5 +1,8 @@
-create or replace force view logger_logs_60_min as
-	select * 
-      from logger_logs 
-	 where time_stamp > systimestamp - (1/24)
+create or replace force view logger_logs_60_min 
+as
+	select /*+ qb_name(logger_logs_60_min) */
+    ll.* 
+	from logger_logs ll
+	where 1=1
+    and ll.time_stamp > cast(systimestamp as timestamp) - (1/24)
 /
