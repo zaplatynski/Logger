ab57b9a2177e9b192a80e54d5881f8549a831897
Martin D'Souza <martin@clarifit.com>
2018-06-03 18:32:41 -0600

    #184 fixed bug to allow for `GLOBAL_CONTEXT_NAME` to be stored in `logger_prefs`

--- contexts/logger_context.sql
+++ contexts/logger_context.sql
@@ -1,11 +1,15 @@
 declare
 	-- the following line is also used in a constant declaration in logger.pkb
-	l_ctx_name varchar2(35) := substr(sys_context('USERENV','CURRENT_SCHEMA'),1,23)||'_LOGCTX';
+	-- l_ctx_name varchar2(35) := substr(sys_context('USERENV','CURRENT_SCHEMA'),1,23)||'_LOGCTX';
+	l_sql varchar2(500);
 begin
-	execute immediate 'create or replace context '||l_ctx_name||' using logger accessed globally';
+	l_sql := 'create or replace context %s1 using logger accessed globally';
+	execute immediate logger.sprintf(l_sql, logger.g_context_name);
 
+	-- #184: hack to allow for setting GLOBAL_CONTEXT_NAME
+	logger.g_can_update_logger_prefs := true;
 	merge into logger_prefs p
-	using (select 'GLOBAL_CONTEXT_NAME' pref_name, l_ctx_name pref_value, logger.g_pref_type_logger pref_type from dual) d
+	using (select 'GLOBAL_CONTEXT_NAME' pref_name, logger.g_context_name pref_value, logger.g_pref_type_logger pref_type from dual) d
 		on (1=1
 			and p.pref_type = d.pref_type
 			and p.pref_name = d.pref_name)
@@ -14,5 +18,7 @@ begin
 	when not matched then
 		insert (p.pref_name, p.pref_value, p.pref_type)
 		values (d.pref_name, d.pref_value, d.pref_type);
+	logger.g_can_update_logger_prefs := false;
+
 end;
 /
