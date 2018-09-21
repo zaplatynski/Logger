3c4ee1339f961db9a7ccb3a0e3f89594e2775672
Martin D'Souza <martin@clarifit.com>
2018-09-21 05:16:18 -0600

    #219 explicit column names in views as well as re-ordered so scope appears first

--- views/logger_logs_5_min.sql
+++ views/logger_logs_5_min.sql
@@ -1,8 +1,23 @@
 create or replace force view logger_logs_5_min 
 as
-	select /*+ qb_name(logger_logs_5_min) */
-    ll.* 
-	from logger_logs ll
-	where 1=1
+  select /*+ qb_name(logger_logs_5_min) */
+    ll.id,
+    ll.logger_level,
+    ll.scope,
+    ll.text,
+    ll.time_stamp,
+    ll.module,
+    ll.action,
+    ll.user_name,
+    ll.client_identifier,
+    ll.call_stack,
+    ll.unit_name,
+    ll.line_no,
+    ll.scn,
+    ll.extra,
+    ll.sid,
+    ll.client_info
+  from logger_logs ll
+  where 1=1
     and ll.time_stamp > cast(systimestamp as timestamp) - (5/1440)
 /
