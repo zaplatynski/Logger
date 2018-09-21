cb316bdacb019313c9e039a6a850ba601c403833
Martin D'Souza <martin@clarifit.com>
2018-09-21 05:17:23 -0600

    minor: code formatting

--- views/logger_logs_terse.sql
+++ views/logger_logs_terse.sql
@@ -1,11 +1,14 @@
 set termout off
 -- setting termout off as this view will install with an error as it depends on logger.date_text_format
 create or replace force view logger_logs_terse as
- select id, logger_level, 
-        substr(logger.date_text_format(time_stamp),1,20) time_ago,
-        substr(text,1,200) text
-   from logger_logs
-  where time_stamp > systimestamp - (5/1440)
+  select 
+    id,
+    logger_level, 
+    substr(logger.date_text_format(time_stamp),1,20) time_ago,
+    substr(text,1,200) text
+  from logger_logs
+  where 1=1
+    and time_stamp > systimestamp - (5/1440)
   order by id asc
 /
 
